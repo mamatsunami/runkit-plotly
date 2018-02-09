e80d610b481fe2a6d855f4303a7c2de302a46969
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-02-09 16:42:56 +0000

    Add unique ID to divs to support multiple ploltly graphs per notebook

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
  
 module.exports = (plotlyFn, options = {}) => {
 	const plotlyLocation = 'https://cdn.plot.ly/plotly-latest.min.js';
+	const id = Math.random().toString(36).substr(2, 10);
 	options = Object.assign({
 		width: 550,
 		height: 450,
@@ -16,10 +17,10 @@ module.exports = (plotlyFn, options = {}) => {
 		return `
 			<center>
 				<style>${options.style}</style>
-				<div id="plotly" width=${options.width} height=${options.height}></div>
+				<div id="plotly-${id}" width=${options.width} height=${options.height}></div>
 				<script src="${plotlyLocation}"></script>
 				<script>
-					(${plotlyFn.toString()})(${JSON.stringify(plotlyArgs)}, "plotly", Plotly)
+					(${plotlyFn.toString()})(${JSON.stringify(plotlyArgs)}, "plotly-${id}", Plotly)
 				</script>
 			</center>
 		`;
